45327e60b7fff2c2451e5426137258f1c9c7c8a2
Bayasaa <bayarbayasgalan.j@gmail.com>
2024-02-03 16:49:06 -0600

    fac icon

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -11,5 +12,5 @@ urlpatterns = [
     path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
     path('add_record/', views.add_record, name='add_record'),
     path('update_record/<int:pk>', views.update_record, name='update_record'),
-
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.ico')))
 ]
